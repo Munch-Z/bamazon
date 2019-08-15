cc843c33af019239ec9ad3188b0a78801c4ad47b
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-08-14 17:32:41 -0700

    Inquire done.

--- bamazon.js
+++ bamazon.js
@@ -21,19 +21,62 @@ fs.readFile('test.txt', 'utf8', (err, data) => {
 })
 
 
+let bamazonDB;
 
 function app(obj){
-    let bamazonDB = mysql.createConnection(obj);
+    bamazonDB = mysql.createConnection(obj);
     bamazonDB.connect();
 
     bamazonDB.query('SELECT * FROM products', (err, res, fields) => {
-        if (err) throw err;
+        if (err) return console.log(err);
 
         res.forEach((e) => {
            const {item_id, product_name, department_name, price, stock_quantity} = e;
            console.table({ID: item_id, Product: product_name, Dept: department_name, Price: price, Quantity: stock_quantity});
         })
 
+        buyPrompt();
+
         bamazonDB.end();
     })
+}
+
+function buyPrompt() {
+    inquirer.prompt([{
+        name: 'item_id',
+        type: 'input',
+        message: 'What is the ID of the item that you would like to buy?',
+        validate: function(value) {
+            let num = parseInt(value);
+
+            if(isNaN(num)) {
+                console.log('\n That is not a valid number. Please try again.');
+                return false;
+            }
+
+            return true;
+        }
+    },
+    {
+        name:'stock_quantity',
+        type: 'input',
+        message: 'How many would you like to buy?',
+        validate: function(value) {
+            let num = parseInt(value);
+
+            if(isNaN(num)) {
+                console.log('\n That is not a valid number. Please try again.');
+                return false;
+            }
+
+            return true;
+        }
+    }]).then((data) => {
+        console.log(data);
+        // buyQuery(obj);
+    })
+}
+
+function buyQuery(queryParams) {
+    //TODO
 }
